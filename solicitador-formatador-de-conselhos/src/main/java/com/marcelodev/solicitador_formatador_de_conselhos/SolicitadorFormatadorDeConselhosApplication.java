f10fc52d5f5e4283f391b0c992defb3cee84c072
marcelojacint <marcelojacintoferreirq@gmail.com>
2024-11-19 00:00:49 -0300

    feat: projeto inicial

---
+++ solicitador-formatador-de-conselhos/src/main/java/com/marcelodev/solicitador_formatador_de_conselhos/SolicitadorFormatadorDeConselhosApplication.java
@@ -0,0 +1,13 @@
+package com.marcelodev.solicitador_formatador_de_conselhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitadorFormatadorDeConselhosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitadorFormatadorDeConselhosApplication.class, args);
+	}
+
+}
---
+++ solicitador-formatador-de-conselhos/src/test/java/com/marcelodev/solicitador_formatador_de_conselhos/SolicitadorFormatadorDeConselhosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelodev.solicitador_formatador_de_conselhos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolicitadorFormatadorDeConselhosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
