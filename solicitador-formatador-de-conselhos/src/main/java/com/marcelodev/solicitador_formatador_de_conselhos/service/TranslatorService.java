0de456042ffef6736623a8b70e3388cff79741f3
marcelojacint <marcelojacintoferreirq@gmail.com>
2024-11-23 00:17:54 -0300

    config: setando a chave de acesso da api de acesso em variavel de ambiente

--- solicitador-formatador-de-conselhos/src/main/java/com/marcelodev/solicitador_formatador_de_conselhos/service/TranslatorService.java
+++ solicitador-formatador-de-conselhos/src/main/java/com/marcelodev/solicitador_formatador_de_conselhos/service/TranslatorService.java
@@ -1,5 +1,6 @@
 package com.marcelodev.solicitador_formatador_de_conselhos.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.google.cloud.translate.Translate;
@@ -9,21 +10,21 @@ import com.google.cloud.translate.Translation;
 @Service
 public class TranslatorService {
 
-	
+	@Value("${google.api.key}")
+	private String apiKey;
 
 	private Translate translate;
 
 	public TranslatorService() {
 		this.translate = TranslateOptions.getDefaultInstance().getService();
+
 	}
 
 	public String translateToPortuguese(String textToTranslate) {
-		
-		 Translate translate = TranslateOptions.newBuilder()
-		            .setApiKey("AIzaSyDa2sI4_nbNAomkNaLw0BxFO5CG71iEi8U") // Substitua pela sua chave de API
-		            .build()
-		            .getService();
-		
+
+		Translate translate = TranslateOptions.newBuilder().setApiKey(apiKey) 
+				.build().getService();
+
 		Translation translation = translate.translate(textToTranslate, Translate.TranslateOption.sourceLanguage("en"),
 				Translate.TranslateOption.targetLanguage("pt"));
 
